a563731d77d873e130d1bd391b0ceb8b70867594
fulei <fulei@yongche.com>
2017-03-11 15:10:13 +0800

    fix conflict

--- app/models/cart.go
+++ app/models/cart.go
@@ -9,3 +9,24 @@ type Cart struct {
 	Cart_price    int    `xorm:"int"`
 	Creattime     int64  `xorm:"int"`
 }
+
+func (c *Cart) Save(cart Cart) bool {
+	has, err := DB_Write.Table("dl_cart").Insert(cart)
+	if err != nil {
+		revel.WARN.Println(has)
+		revel.WARN.Println("错误: %v", err)
+		return false
+	}
+	return true
+}
+
+func (c *Cart) Delete(cartId int) bool {
+	cart := new(Cart)
+	has, err := DB_Write.Table("dl_cart").Where("cart_id").Delete(cart)
+	if err != nil {
+		revel.WARN.Println(has)
+		revel.WARN.Println("错误: %v", err)
+		return false
+	}
+	return true
+}
