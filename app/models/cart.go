445d9ca9c594d736daee703f404d273437716a10
fulei <fulei@yongche.com>
2017-03-11 15:48:07 +0800

    fix conflcit

--- app/models/cart.go
+++ app/models/cart.go
@@ -7,6 +7,7 @@ type Cart struct {
 	Cart_pro_name string `xorm:"varchar(11)"`
 	Cart_pro_num  int    `xorm:"int"`
 	Cart_price    int    `xorm:"int"`
+	Cart_pro_id   int    `xorm:"int"`
 	Creattime     int64  `xorm:"int"`
 }
 
@@ -23,6 +24,16 @@ func (c *Cart) Save(cart Cart) bool {
 func (c *Cart) Delete(cartId int) bool {
 	cart := new(Cart)
 	has, err := DB_Write.Table("dl_cart").Where("cart_id").Delete(cart)
+	if err != nil {
+		revel.WARN.Println(has)
+		revel.WARN.Println("错误: %v", err)
+		return false
+	}
+	return true
+}
+
+func (c *Cart) Upadte(cartId int, cart Cart) bool {
+	has, err := DB_Write.Table("dl_sys_role").Id(cartId).Update(cart)
 	if err != nil {
 		revel.WARN.Println(has)
 		revel.WARN.Println("错误: %v", err)
